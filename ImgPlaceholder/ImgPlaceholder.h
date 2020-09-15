82d8cff1ebf0922faf0925298261e271466b3979
shyam-kumar-iOS <shyamkumarios1070@gmail.com>
2020-09-15 17:18:49 +0530

    Initial proj setup

---
+++ ImgPlaceholder/ImgPlaceholder.h
@@ -0,0 +1,19 @@
+//
+//  ImgPlaceholder.h
+//  ImgPlaceholder
+//
+//  Created by Shyam Kumar on 9/15/20.
+//  Copyright © 2020 Shyam Kumar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImgPlaceholder.
+FOUNDATION_EXPORT double ImgPlaceholderVersionNumber;
+
+//! Project version string for ImgPlaceholder.
+FOUNDATION_EXPORT const unsigned char ImgPlaceholderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImgPlaceholder/PublicHeader.h>
+
+
---
+++ ImgPlaceholderExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImgPlaceholderExample
+//
+//  Created by Shyam Kumar on 9/15/20.
+//  Copyright © 2020 Shyam Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
